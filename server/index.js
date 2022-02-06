61acd2dbe1e9a31beed1bdd38403c33749d5bba3
teo00471 <mcardonaca@unal.edu.co>
2022-02-06 10:55:16 -0500

    Servidor

--- server/index.js
+++ server/index.js
@@ -1,13 +1,61 @@
+const { response } = require('express');
+
 const express = require('express');
+
+var mysql = require('mysql')
 
 const app = express()
 
 const port = 3000
 
 
-app.get('/', (req, res)=>{
-    res.send('Hola, morcho')
-})
+
+var dbconn = mysql.createConnection({
+    host     : 'l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user     : 'b7ar714azfw9xpe4',
+    password : 'zgptgw4l9kic26iu',
+    database : 'xsnqk4f1ukcjtji2',
+    port : 3306
+  });
+  
+  dbconn.connect(function(err){
+    if(err){
+      console.log('Database connection error', err);
+    }else{
+      console.log('Database connection successful');
+    }
+  });
+
+
+
+
+
+
+
+// const config = {
+//     host: 'l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     user: 'b7ar714azfw9xpe4',
+//     password: 'zgptgw4l9kic26iu ',
+//     database: 'xsnqk4f1ukcjtji2',
+//     port: 3306
+// };
+
+dbconn.connect(function(err){
+    if(err){
+      console.log('Database connection error');
+    }else{
+      console.log('Database connection successful');
+    }
+  });
+   
+
+app.get('/obtenerDatos', (req, res) => {
+    dbconn.query('SELECT * FROM users', (error, result) => {
+        if (error) throw error;
+
+        res.send(result);
+    });
+});
 
 app.get('/api', (req, res)=>{
     res.send('Hola, gay')
@@ -21,3 +69,6 @@ app.get('/insertar',(req,res)=>{
 app.listen(port, ()=> {
     console.log(`Api corriendo en el puerto ${port}`)
 })
+
+
+
